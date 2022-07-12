81df905f2f8cbc64073aa9f4e24340e497cde160
Nicole Olliver <noliver.ykc@gmail.com>
2022-07-12 20:27:08 +0900

    fixed routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  # cloudnary
+  config.active_storage.service = :cloudinary
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: 'lists#index'
   #lists + bookmarks
   resources :lists, only: [:index, :create, :show, :new] do
     resources :bookmarks, only: [:new, :create]
